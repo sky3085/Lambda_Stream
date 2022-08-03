391f66c649787614c4034738f4bf95e9e77e2dc3
yeonghwan Moon <skysky3085@daum.net>
2022-08-03 23:24:18 +0900

    람다 연습

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.*;
 
 public class Main {
+    private static List<Student> list=Arrays.asList(
+            new Student("홍길동", 90, 97),
+            new Student("문영환", 100, 100)
+            );
+
+    public static void printString(Function<Student, String> function){
+        for (Student stud : list) {
+            System.out.print(function.apply(stud) + " ");
+        }
+        System.out.println();
+    }
+
+
     public static void main(String[] args) {
         String m1 = "Lambda Practice ";
         String m2 = "Java 8";
@@ -18,5 +33,9 @@ public class Main {
         int num = intSupplier.getAsInt();
         System.out.println("num = " + num);
 
+        System.out.println("[학생이름]");
+        printString(t->t.getName());
     }
+
+
 }
